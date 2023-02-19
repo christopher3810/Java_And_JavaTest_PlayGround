3144dc7be5df413218979ba72c4a3d1df9988c9b
sungminyang <lonesome661@naver.com>
2023-02-19 16:43:58 +0900

    java_feature test project init with out db

---
+++ Java_Feature_Test/test/src/main/java/com/javafeature/test/TestApplication.java
@@ -0,0 +1,15 @@
+package com.javafeature.test;
+
+import javax.sql.DataSource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
