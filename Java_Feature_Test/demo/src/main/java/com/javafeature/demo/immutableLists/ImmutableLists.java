79c8e115f9b6679e13a523a8bf25f5530932249e
sungminyang <lonesome661@naver.com>
2023-04-30 19:03:35 +0900

    unmodified list test code 추가

---
+++ Java_Feature_Test/demo/src/main/java/com/javafeature/demo/immutableLists/ImmutableLists.java
@@ -0,0 +1,8 @@
+package com.javafeature.demo.immutableLists;
+
+public class ImmutableLists {
+
+    public void test(){
+        //.. to test code
+    }
+}
---
+++ Java_Feature_Test/demo/src/test/java/com/javafeature/demo/immutableLists/ImmutableListsTest.java
@@ -0,0 +1,79 @@
+package com.javafeature.demo.immutableLists;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class ImmutableListsTest {
+
+    @Test
+    public void test() {
+
+        List<String> modifiableList = new ArrayList<String>();
+        modifiableList.add("a");
+
+        System.out.println("modifiableList:"+modifiableList);
+        System.out.println("--");
+
+
+        //unModifiableList
+
+        assertEquals(1, modifiableList.size());
+
+        List<String> unModifiableList= Collections.unmodifiableList(
+            modifiableList);
+
+        modifiableList.add("b");
+
+        boolean exceptionThrown=false;
+        try {
+            unModifiableList.add("b");
+            fail("add supported for unModifiableList!!");
+        } catch (UnsupportedOperationException e) {
+            exceptionThrown=true;
+            System.out.println("unModifiableList.add() not supported");
+        }
+        assertTrue(exceptionThrown);
+
+        System.out.println("modifiableList:"+modifiableList);
+        System.out.println("unModifiableList:"+unModifiableList);
+
+        assertEquals(2, modifiableList.size());
+        assertEquals(2, unModifiableList.size());
+        System.out.println("--");
+
+
+
+        //immutableList
+
+
+        List<String> immutableList=Collections.unmodifiableList(
+            new ArrayList<String>(modifiableList));
+
+        modifiableList.add("c");
+
+        exceptionThrown=false;
+        try {
+            immutableList.add("c");
+            fail("add supported for immutableList!!");
+        } catch (UnsupportedOperationException e) {
+            exceptionThrown=true;
+            System.out.println("immutableList.add() not supported");
+        }
+        assertTrue(exceptionThrown);
+
+
+        System.out.println("modifiableList:"+modifiableList);
+        System.out.println("unModifiableList:"+unModifiableList);
+        System.out.println("immutableList:"+immutableList);
+        System.out.println("--");
+
+        assertEquals(3, modifiableList.size());
+        assertEquals(3, unModifiableList.size());
+        assertEquals(2, immutableList.size());
+
+    }
+}
