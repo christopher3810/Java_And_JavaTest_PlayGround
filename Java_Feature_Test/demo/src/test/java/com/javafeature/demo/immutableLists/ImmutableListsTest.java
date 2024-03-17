1988e768d56eb604b1495da35c565e021dec4aa3
sungminyang <lonesome661@naver.com>
2024-03-17 19:09:25 +0900

    [Refactor] : assertJ 활용 테스트코드 리팩터링
    불필요한 assertThat 전부 수정.
    test 메서드 분할
    nestedClass, test method로 분리

--- Java_Feature_Test/demo/src/test/java/com/javafeature/demo/immutableLists/ImmutableListsTest.java
+++ Java_Feature_Test/demo/src/test/java/com/javafeature/demo/immutableLists/ImmutableListsTest.java
@@ -1,94 +1,77 @@
 package com.javafeature.demo.immutableLists;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.google.common.collect.ImmutableList;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
 
 class ImmutableListsTest {
 
-    @Test
-    public void test() {
 
-        List<String> modifiableList = new ArrayList<String>();
-        modifiableList.add("a");
+    @Nested
+    public class list_invariants_test {
+        @DisplayName("immutableList 값 추가 되는지 테스트")
+        @Test
+        void immutableListTest() {
 
-        System.out.println("modifiableList:"+modifiableList);
-        System.out.println("--");
+            List<String> modifiableList = new ArrayList<>();
+            modifiableList.add("a");
 
+            List<String> immutableList= List.copyOf(modifiableList);
 
-        //unModifiableList
+            modifiableList.add("c");
 
-        assertEquals(1, modifiableList.size());
+            assertThatThrownBy(() -> immutableList.add("c"))
+                .as("Check immutableList modification")
+                .isInstanceOf(UnsupportedOperationException.class);
 
-        List<String> unModifiableList= Collections.unmodifiableList(
-            modifiableList);
+            assertThat(modifiableList).hasSize(2);
+            assertThat(immutableList).hasSize(1);
 
-        modifiableList.add("b");
-
-        boolean exceptionThrown=false;
-        try {
-            unModifiableList.add("b");
-            fail("add supported for unModifiableList!!");
-        } catch (UnsupportedOperationException e) {
-            exceptionThrown=true;
-            System.out.println("unModifiableList.add() not supported");
         }
-        assertTrue(exceptionThrown);
 
-        System.out.println("modifiableList:"+modifiableList);
-        System.out.println("unModifiableList:"+unModifiableList);
+        @DisplayName("unmodifiedList 에 값 추가되는지 테스트")
+        @Test
+        void unmodifiedListTest(){
+            //given
+            List<String> modifiableList = new ArrayList<>();
+            modifiableList.add("a");
 
-        assertEquals(2, modifiableList.size());
-        assertEquals(2, unModifiableList.size());
-        System.out.println("--");
+            List<String> unModifiableList= Collections.unmodifiableList(
+                modifiableList);
 
+            modifiableList.add("b");
 
+            //when then
+            assertThatThrownBy(() -> unModifiableList.add("b"))
+                .as("Check unmodifiableList modification")
+                .isInstanceOf(UnsupportedOperationException.class);
+            assertThat(modifiableList).hasSize(2);
+            assertThat(unModifiableList).hasSize(2);
 
-        //immutableList
-
-
-        List<String> immutableList=Collections.unmodifiableList(
-            new ArrayList<String>(modifiableList));
-
-        modifiableList.add("c");
-
-        exceptionThrown=false;
-        try {
-            immutableList.add("c");
-            fail("add supported for immutableList!!");
-        } catch (UnsupportedOperationException e) {
-            exceptionThrown=true;
-            System.out.println("immutableList.add() not supported");
         }
-        assertTrue(exceptionThrown);
-
-
-        System.out.println("modifiableList:"+modifiableList);
-        System.out.println("unModifiableList:"+unModifiableList);
-        System.out.println("immutableList:"+immutableList);
-        System.out.println("--");
-
-        assertEquals(3, modifiableList.size());
-        assertEquals(3, unModifiableList.size());
-        assertEquals(2, immutableList.size());
-
     }
 
     @Test
     public void givenUsingJava9_whenNullElement_thenThrowsException() {
         List<String> list = new ArrayList<>(Arrays.asList("one", "two", "three", null));
-        assertThrows(NullPointerException.class, () -> List.of(list.toArray(new String[]{})));
+        assertThatThrownBy(() -> List.of(list.toArray(new String[0])))
+            .isInstanceOf(NullPointerException.class);
     }
 
     @Test
     public void givenUsingGuava_whenNullElement_thenNoException() {
         List<String> list = new ArrayList<>(Arrays.asList("one", "two", "three", null));
-        assertThrows(NullPointerException.class, () -> ImmutableList.copyOf(list).add("four"));
+        assertThatThrownBy(() -> ImmutableList.copyOf(list).add("four"))
+            .isInstanceOf(NullPointerException.class);
     }
 
     @Test
@@ -97,11 +80,9 @@ class ImmutableListsTest {
         List<String> unmodifiableList = Collections.unmodifiableList(list);
         list.add("four");
 
-        assertEquals(4, unmodifiableList.size());
-        assertEquals("one", unmodifiableList.get(0));
-        assertEquals("two", unmodifiableList.get(1));
-        assertEquals("three", unmodifiableList.get(2));
-        assertEquals("four", unmodifiableList.get(3));
+        assertThat(unmodifiableList)
+            .hasSize(4)
+            .containsExactly("one", "two", "three", "four");
     }
 
 
